3006e6a3533968ae8aa9551d415873531200c920
VladyslavKosarenko <131978971+VladyslavKosarenko@users.noreply.github.com>
2023-08-30 23:56:14 +0300

    part-one

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,51 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+function partsGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </li>
+        `;
+        
+    }).join('');
+}
+
+const ul = document.querySelector('.gallery');
+const markup = partsGallery(galleryItems);
+
+ul.insertAdjacentHTML('beforeend', markup);
+ul.addEventListener('click', onClick);
+function onClick(event) {
+
+    if (!event.target.classList.contains('gallery__image')) {
+        return; 
+    }
+
+    const original = event.target.dataset.source;
+    const description = event.target.alt;
+    console.log(event.target.dataset.source);
+    // modal
+const instance = basicLightbox.create(`
+    <div class="modal">
+    <img
+                    class="gallery__image"
+                    src="${original}"
+                    data-source="${original}"
+                    alt="${description}"
+                /> 
+    </div>
+`)
+
+instance.show()
+}
+
